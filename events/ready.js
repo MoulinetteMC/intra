8b72d11603d4fe5bc65de5b7dc60c271ae0b1611
Valentin_Kh <valentin_kh@outlook.fr>
2025-12-16 17:36:47 +0100

    Remove idle on development state

--- events/ready.js
+++ events/ready.js
@@ -11,18 +11,11 @@ module.exports = {
 	async execute(client) {
 		console.log(`Ω ${client.user.tag} is online !`.green);
 
-		if (os.hostname() !== "PC-Valentin") {
-			client.user.setPresence({
-				status: "online",
-				activities: [
-					{ name: `submits of practicals`, type: ActivityType.Watching },
-				],
-			});
-		} else {
-			client.user.setPresence({
-				status: "idle",
-				activities: [{ name: `0% archi tags`, type: ActivityType.Watching }],
-			});
-		}
+		client.user.setPresence({
+			status: "online",
+			activities: [
+				{ name: `submits of practicals`, type: ActivityType.Watching },
+			],
+		});
 	},
 };
